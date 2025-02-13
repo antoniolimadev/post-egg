04d55b797a2290bc6e782146f4686c4c90cda3d9
Antonio <limaa91@gmail.com>
2025-02-13 12:39:15 +0000

    resolves #3

--- resources/views/livewire/launcher.blade.php
+++ resources/views/livewire/launcher.blade.php
@@ -1,18 +1,21 @@
 <div
-    x-data @click.away="$wire.relaunch()"
+    x-data="{ isFocused: false }"
+    @click.away="$wire.relaunch(); isFocused = false;"
     class="pe-c-launcher"
 >
-
     <p>
         <input
             type="text"
             wire:model.live.debounce="title"
+            @focus="isFocused = true"
             placeholder="Post it..."
         >
     </p>
 
     <p>
         <textarea
+            x-show="isFocused"
+            x-transition.duration.300ms
             wire:model.live.debounce="description"
             placeholder="Description"
             rows="5"
