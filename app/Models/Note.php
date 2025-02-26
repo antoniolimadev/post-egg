6451ca1fa2b019a1e786bab29934405132032e4c
Antonio <limaa91@gmail.com>
2025-02-26 21:44:18 +0000

    save session id for every note that is created*
    *create migration to add session id to notes table
    *create global scope to to always filter out notes based on session id if the user is a demo user

--- app/Models/Note.php
+++ app/Models/Note.php
@@ -2,17 +2,31 @@
 
 namespace App\Models;
 
+use App\Models\Scopes\SessionScope;
+use Illuminate\Database\Eloquent\Attributes\ScopedBy;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Carbon;
 
+#[ScopedBy([SessionScope::class])]
 class Note extends Model
 {
     use HasFactory;
 
     public $fillable = [
         'archived_at',
+        'session_id',
     ];
+
+    protected static function booted(): void
+    {
+        static::created(function (Note $note) {
+            $note->update([
+                'session_id' => session()->getId(),
+            ]);
+        });
+
+    }
 
     public function archive(): void
     {
